0a4c48a90f9f576f47ddceb7c7edca9563a9840f
kPaNN8874 <113461676+kPaNN8874@users.noreply.github.com>
2023-09-19 12:06:46 +0100

    Add files via upload

---
+++ WpfApp1/Fuvar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Fuvar
+    {
+        private int taxiAzonosito;
+        private string indulasiIdopont;
+        private int utazasIdotartama;
+        private float megtettTavolsag;
+        private float viteldij;
+        private float borravaló;
+        private string fizetesModja;
+
+        
+
+        public Fuvar(int taxiAzonosito, string indulasiIdopont, int utazasIdotartama, float megtettTavolsag, float viteldij, float borravaló, string fizetesModja)
+        {   
+            this.taxiAzonosito = taxiAzonosito;
+            this.indulasiIdopont = indulasiIdopont;
+            this.utazasIdotartama = utazasIdotartama;
+            this.megtettTavolsag = megtettTavolsag;
+            this.viteldij = viteldij;
+            this.borravaló = borravaló;
+            this.fizetesModja = fizetesModja;
+        }
+        public int TaxiAzonosito { get => taxiAzonosito;}
+        public string IndulasiIdopont { get => indulasiIdopont;}
+        public int UtazasIdotartama { get => utazasIdotartama; }
+        public float MegtettTavolsag { get => megtettTavolsag; }
+        public float Viteldij { get => viteldij; }
+        public float Borravaló { get => borravaló; }
+        public string FizetesModja { get => fizetesModja; }
+
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.DirectoryServices.ActiveDirectory;
+using System.Diagnostics.Metrics;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Fuvar> fuvarok = File.ReadAllLines("fuvar.csv").Skip(1).Select(x=>new Fuvar(x) ).toList();
+
+        }
+
+        private void harmasFeladat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show($"3.feladat:{fuvarok.Count()} fuvar");
+        }
+
+        private void negyesFeladat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show($"4. feladat: {fuvarok.where(x => x.taxiAzonosito).Count()} fuvar alatt: {fuvarok.Sum().Where(x => x.viteldij} "); 
+        }
+
+        private void otosFeladat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show($"5. feladat:{fuvarok.groupBy(fizetesModja).}");
+        }
+
+        private void hatosFeladat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show($"6. feladat : {fuvarok.Where(x => x.megtettTavolsag).Sum() * 1.6} km");
+        }
+
+        private void nyolcasFeladat_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
